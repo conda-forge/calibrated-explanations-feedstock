f5891409b742d97c8306415c62c0a8a2b431cffe
Tuwe Löfström <tuwe.lofstrom@ju.se>
2024-11-28 08:54:09 +0100

    Add CI workflow to update meta.yaml

--- update_meta.py
+++ update_meta.py
@@ -1,28 +1,30 @@
 import requests
-import yaml
 import re
 
 # Fetch the latest version from PyPI
 response = requests.get('https://pypi.org/pypi/calibrated-explanations/json')
 data = response.json()
 latest_version = data['info']['version']
-sha256_url = f"https://pypi.org/project/calibrated-explanations/{latest_version}/#files"
-sha256_response = requests.get(sha256_url)
-sha256_match = re.search(r'sha256=([a-f0-9]{64})', sha256_response.text)
-latest_sha256 = sha256_match.group(1) if sha256_match else None
 
-if not latest_sha256:
-    raise ValueError("Could not find sha256 hash on PyPI page")
+# Fetch the sha256 hash from the release files
+release_files = data['releases'][latest_version]
+sha256_hash = None
+for file_info in release_files:
+    if file_info['filename'].endswith('.tar.gz'):
+        sha256_hash = file_info['digests']['sha256']
+        break
+
+if not sha256_hash:
+    raise ValueError("Could not find sha256 hash for the latest version on PyPI")
 
 # Read the existing meta.yaml file
-with open('meta.yaml', 'r') as file:
-    meta = yaml.safe_load(file)
+with open('recipe/meta.yaml', 'r') as file:
+    meta_content = file.read()
 
-# Update the version and sha256
-meta['package']['version'] = latest_version
-meta['source']['url'] = f"https://pypi.io/packages/source/c/calibrated-explanations/calibrated_explanations-{latest_version}.tar.gz"
-meta['source']['sha256'] = latest_sha256
+# Update the version and sha256 in the Jinja2 template
+meta_content = re.sub(r'{% set version = ".*?" %}', f'{{% set version = "{latest_version}" %}}', meta_content)
+meta_content = re.sub(r'sha256: .*', f'sha256: {sha256_hash}', meta_content)
 
 # Write the updated meta.yaml file
-with open('meta.yaml', 'w') as file:
-    yaml.dump(meta, file, default_flow_style=False)
+with open('recipe/meta.yaml', 'w') as file:
+    file.write(meta_content)
